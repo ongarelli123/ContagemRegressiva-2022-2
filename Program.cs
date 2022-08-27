82223779e83e25f7aa9accf64150a3ef700e35bb
joao  Victor <ongarelli1994@gmail.com>
2022-08-26 22:43:27 -0300

    salvo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Preparar...");
+Console.ReadKey();
+Console.WriteLine("Apontar...");
+console.Readley();
+Console.WriteLine("Fogo...");
+
